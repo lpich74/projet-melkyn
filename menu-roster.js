f933f8baa6481d8cd5fd686f47be2e6c9223b141
Lucas PICHOLLET <Lucas@MacBook-Pro-de-Lucas.local>
2023-12-07 17:16:05 +0100

    menu-roster disappearing fixing

--- menu-roster.js
+++ menu-roster.js
@@ -1,8 +1,17 @@
 const buttonRoster = document.getElementById('open-roster');
 const openListRoster = document.querySelectorAll('.element-roster');
 
-buttonRoster.addEventListener("click", function() {
+buttonRoster.addEventListener("click", function(event) {
+    event.stopPropagation();
     openListRoster.forEach(function(element) {
         element.classList.toggle("hidden");
+    });
+});
+
+document.addEventListener("click", function() {
+    openListRoster.forEach(function(element) {
+        if (!element.classList.contains("hidden")) {
+            element.classList.add("hidden");
+        }
     });
 });
